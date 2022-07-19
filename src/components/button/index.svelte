cd6251b148fe173400f9b06035adbbb593d03551
pppinsuni <59594326+pppinsuni@users.noreply.github.com>
2022-07-20 07:00:28 +0900

    start

---
+++ src/components/button/index.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  export let text : string;
+  export let onClick : ()=>void;
+  // 함수 타입 
+  export let color : 'orange' | 'grey'| 'dark-grey';
+</script>
+
+<div class="button" data-color={color} 
+onclick={()=> onClick(text)}>{text}</div>
+
+<style lang="scss">
+  .button {
+    width: 80px;
+    height: 80px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 24px;
+    font-weight: 600;
+    &[data-color='orange'] {
+      background-color:orange;
+      // 계층적 표현이 가능
+    }
+    &[data-color='darkgrey'] {
+      background-color:darkgrey;
+      // 계층적 표현이 가능
+    }
+    &[data-color='grey'] {
+      background-color:grey;
+      // 계층적 표현이 가능
+    }
+  }
+</style>
---
+++ src/components/button/index.ts
@@ -0,0 +1,2 @@
+export {default as Button} from './index.svelte';
+// 내보낼 이름
